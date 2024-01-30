4c741c7fc9040fe6c3f98ddde15724f06a387062
Lisacyj0102 <s1110169@my.cmsh.cyc.edu.tw>
2024-01-30 10:41:29 +0800

    add robotContainer

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -1,5 +1,7 @@
 package frc.robot;
 
 public class Constants {
-    
+    public static class XboxControllerPortConstants {
+        public static final int Xbox = 0;
+    }
 }
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -6,13 +6,38 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import frc.robot.Command.IntakeCmd;
+import frc.robot.Command.ShooterCmd;
+import frc.robot.Constants.XboxControllerPortConstants;
+import frc.robot.Subsystem.ArmSubsystem;
+import frc.robot.Subsystem.ClimbingSubsystem;
+import frc.robot.Subsystem.IntakeSubsystem;
+import frc.robot.Subsystem.ShooterSubsystem;
 
 public class RobotContainer {
+  CommandXboxController XboxController;
+
+  ArmSubsystem armSubsystem;
+  ClimbingSubsystem climbingSubsystem;
+  IntakeSubsystem intakeSubsystem;
+  ShooterSubsystem shooterSubsystem;
+
   public RobotContainer() {
+    XboxController = new CommandXboxController(XboxControllerPortConstants.Xbox);
+
+    armSubsystem = new ArmSubsystem();
+    climbingSubsystem = new ClimbingSubsystem();
+    intakeSubsystem = new IntakeSubsystem();
+    shooterSubsystem = new ShooterSubsystem();
+
     configureBindings();
   }
 
-  private void configureBindings() {}
+  private void configureBindings() {
+    XboxController.a().whileTrue(new IntakeCmd(intakeSubsystem));
+    XboxController.x().whileTrue(new ShooterCmd(shooterSubsystem));
+  }
 
   public Command getAutonomousCommand() {
     return Commands.print("No autonomous command configured");
